828ff1bad8ee299a24faa21902b8b1e7f62102cd
Anand <anandbaburajan@gmail.com>
2019-07-19 17:28:56 +0530

    Completed with LSB but not efficient due to compression

--- main.py
+++ main.py
@@ -1,43 +1,43 @@
 # USAGE
-# python main.py -i vid.mp4 -o result.mp4
+# python main.py -i vid.mp4 -v wbSwFU6tY1c
+# Result video => en_vid.mp4
 
-from LSBSteg import*
+from LSBSteg import *
 import numpy as np
 import argparse
 import cv2
 
-def vid_test():
+def main():
     ap = argparse.ArgumentParser()
     ap.add_argument("-i", "--input", required=True, help="Input video file")
-    ap.add_argument("-o", "--output", required=True, help="Output video file")
+    ap.add_argument("-v", "--videoid", required=True, help="11 byte Video ID")
     args = vars(ap.parse_args())
 
     cap = cv2.VideoCapture(args["input"])
 
     fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
-    out = cv2.VideoWriter(args["output"],fourcc, 30.0, (int(cap.get(3)), int(cap.get(4))))
-
+    out = cv2.VideoWriter("en_" + args["input"],fourcc, 30.0, (int(cap.get(3)), int(cap.get(4))))
+    fc=0
     while(cap.isOpened()):
         ret, frame = cap.read()
         if not ret:
             break
+        steg = LSBSteg(frame)
+        pre = steg.decode_text()
+        print(pre)
+        if pre==0:
+            if fc%10==0:
+                frame_encoded = steg.encode_text(args["videoid"])
+                out.write(frame_encoded)
+            else:
+                out.write(frame)
+        else:
+            print("Video ID: ", pre)
             out.write(frame)
-            cv2.imshow('frame',frame)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-
-                cap.release()
-                out.release()
-                cv2.destroyAllWindows()
-
-def main():
-    steg = LSBSteg(cv2.imread("img.png"))
-    img_encoded = steg.encode_text("11_byte_ID_")
-    cv2.imwrite("en_img.png", img_encoded)
-
-    im = cv2.imread("img.png")
-    steg = LSBSteg(im)
-    print("Video ID:",steg.decode_text())
+        fc+=1
+    cap.release()
+    out.release()
+    cv2.destroyAllWindows()
 
 if __name__=="__main__":
     main()
