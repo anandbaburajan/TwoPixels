4a460eccb0a6e0596b523a0c387725d557fb7356
Anand <anandbaburajan@gmail.com>
2019-07-20 19:30:50 +0530

    Function to encode a bit

--- VideoID.py
+++ VideoID.py
@@ -7,3 +7,14 @@ class VideoID():
     def __init__(self, im):
         self.image = im
         self.width, self.height = im.size
+
+    def encode_bit(self, bit):
+        hsv_img = self.image.convert('HSV')
+        if bit == 0:
+            hsv_img.putpixel((0,0), (0,0,0))
+            hsv_img.putpixel((self.width-1, self.height-1), (0,0,255))
+        else:
+            hsv_img.putpixel((0,0), (0,0,255))
+            hsv_img.putpixel((self.width-1, self.height-1), (0,0,0))
+        self = hsv_img.convert('RGB')
+        return self
