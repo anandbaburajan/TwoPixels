0e40666817b538318d4506cb687c13d271585a89
Anand <anandbaburajan@gmail.com>
2019-07-21 18:11:41 +0530

    Main function and usage

--- TwoPixels.py
+++ TwoPixels.py
@@ -8,10 +8,10 @@ class VideoID():
         self.image = im
         self.width, self.height = im.size
 
-    def encode_bit(self, bit):
+    def encode_bit(self, bit):ie
         hsv_img = self.image.convert('HSV')
         pxl = hsv_img.load()
-        if bit == 0:
+        if bit == '0':
             pxl[0,0] = (0,0,0)
             pxl[self.width-1, self.height-1] = (0,0,255)
         else:
@@ -24,7 +24,8 @@ class VideoID():
         hsv_img = self.image.convert('HSV')
         pxl = hsv_img.load()
         a = pxl[0,0][2] - pxl[self.width-1, self.height-1][2]
+        self.image = hsv_img.convert('RGB')
         if a<0:
-            return 0
+            return '0'
         else:
-            return 1
+            return '1'
